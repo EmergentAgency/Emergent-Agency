e2986f18fff9e06450dc84720fe65bcdceefcb94
clinder <chris@false-profit.com>
2014-06-23 23:46:10 -0400

    Created batch files to set tuning files and load matching reason file.

---
+++ EaMidi/EaMidiPC/Run_Crystal.bat
@@ -0,0 +1,5 @@
+del NodeSettings.txt
+copy NodeSettings_Crystal.txt NodeSettings.txt
+start "" "C:\Users\Chris\Documents\MIDI\Somatone_Crystal.rns"
+timeout /t 3
+C:\processing-2.1\processing-java.exe --run --force --sketch="C:\Users\Chris\Documents\EmergentAgencyGit\EaMidi\EaMidiPC" --output=C:\Windows\Temp\Processing
