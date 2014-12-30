284836561302bb9b4c0a845e4d9183d1f87007a9
Chris Linder (from Brick) <chris.linder@gmail.com>
2014-12-30 15:47:14 -0500

    Fixed up batch file for Brick

--- EaMidi/EaMidiPC/Run_Crystal_Brick.bat
+++ EaMidi/EaMidiPC/Run_Crystal_Brick.bat
@@ -1,5 +1,5 @@
 del NodeSettings.txt
 copy NodeSettings_Crystal.txt NodeSettings.txt
-start "" "C:\Documents and Settings\Chris\My Documents\Emergent-Agency\Somatone\Somatone_Crystal_Brick.rns"
+start "" "C:\Documents and Settings\Chris\My Documents\Emergent-Agency\Somatone\Somatone_Crystal.rns"
 timeout /t 3
-C:\processing-2.1\processing-java.exe --run --force --sketch="C:\Documents and Settings\Chris\My Documents\Emergent-Agency\EaMidi\EaMidiPC_Brick" --output=C:\Windows\Temp\Processing
+C:\processing-2.1\processing-java.exe --run --force --sketch="C:\Documents and Settings\Chris\My Documents\Emergent-Agency\EaMidi\EaMidiPC" --output=C:\Windows\Temp\Processing
