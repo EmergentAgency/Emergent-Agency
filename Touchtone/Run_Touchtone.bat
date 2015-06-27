9e6d2023d7665d34d51ccaa35cff3e7b93e8f1b5
clinder <chris@false-profit.com>
2015-06-27 18:09:43 -0400

    Adding touchtone dir with Reason file and batch file to run the project

---
+++ Touchtone/Run_Touchtone.bat
@@ -0,0 +1,3 @@
+start "" "C:\Users\Chris\Documents\EmergentAgencyGit\Touchtone\Touchtone_Crystal_Touch_Cords_2.rns"
+timeout /t 3
+C:\processing-2.2.1\processing-java.exe --run --force --sketch="C:\Users\Chris\Documents\EmergentAgencyGit\InputGraph" --output=C:\Windows\Temp\Processing
