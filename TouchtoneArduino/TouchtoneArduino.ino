a96cc63253b23c8282a0aa2b09fd618fe1d73768
clinder <chris.linder@gmail.com>
2018-06-10 19:39:41 -0400

    Basically works with 2 lights!
    But still needs lots of cleanup

--- TouchtoneArduino/TouchtoneArduino.ino
+++ TouchtoneArduino/TouchtoneArduino.ino
@@ -71,6 +71,206 @@ int g_iDimmerBrightness[NUM_DIMMERS];
 
 
 
+// TEMP_CL - Add PC singal processing nodes from "InputGraph" program
+#define MAX_NUM_SAMPLES 30
+
+class ProcessingNode
+{
+  protected:
+  
+  class ProcessingNode* m_pNodeInput;
+  boolean m_bDerivative;
+  float m_fExponentialSmoothingWeight;
+  int m_iAvgSmoothingNumSamples;
+  float m_fScalingFactor;
+
+  float m_fInput;
+  float m_fProcessingVar;
+  float m_fExponentialSmoothedInput;
+  float m_fAvgSmoothedInput;
+
+  // TEMP_CL
+  //float m_afPastBuffer[MAX_NUM_SAMPLES];
+  //int m_iPastBufferIndex;
+
+  float m_afAvgBuffer[MAX_NUM_SAMPLES];
+  int m_iBufferIndex;
+  float m_fOutput;
+  
+  public:
+  
+  ProcessingNode(class ProcessingNode* pNodeInput, boolean bDerivative, float fExponentialSmoothingWeight, int iAvgSmoothingNumSamples, float fScalingFactor)
+  {
+    m_pNodeInput = pNodeInput;
+    m_bDerivative = bDerivative;
+    m_fExponentialSmoothingWeight = fExponentialSmoothingWeight;
+    m_iAvgSmoothingNumSamples = iAvgSmoothingNumSamples;
+    m_fScalingFactor = fScalingFactor;
+
+    m_iBufferIndex = 0;
+  }
+
+  /* - TEMP_CL - only used on PC to live tune values
+  float GetExponentialSmoothingWeight()
+  {
+    return m_fExponentialSmoothingWeight;
+  }
+
+  int GetAvgSmoothingNumSamples()
+  {
+    return m_iAvgSmoothingNumSamples;
+  }
+
+  void AdjustSmoothing(float fExponentialSmoothingWeight, int iAvgSmoothingNumSamples)
+  {
+    m_fExponentialSmoothingWeight = fExponentialSmoothingWeight;
+    m_iAvgSmoothingNumSamples = iAvgSmoothingNumSamples;
+  }
+  */
+
+  void UpdateFromInput(int iDeltaTimeMS, float fInput)
+  {
+    if(m_bDerivative)
+    {
+      float fOldInput = m_fInput;
+      m_fInput = fInput;
+      m_fProcessingVar = (m_fInput - fOldInput) / iDeltaTimeMS * 1000.0;
+    }
+    else
+    {
+      m_fProcessingVar = fInput;
+    }
+
+    // Smooth the input using a Exponential moving average
+    if(m_fExponentialSmoothingWeight > 0)
+    {
+      m_fExponentialSmoothedInput = m_fProcessingVar * (1.0 - m_fExponentialSmoothingWeight) + m_fExponentialSmoothedInput * m_fExponentialSmoothingWeight;
+    }
+    else
+    {
+      m_fExponentialSmoothedInput = m_fProcessingVar;
+    }
+
+    // TENP_CL
+    //// Past buffer - very much like avg buffer but not variable in number of samples
+    // = (m_iPastBufferIndex + 1) % MAX_NUM_SAMPLES;
+    //m_afPastBuffer[m_iPastBufferIndex] = m_fExponentialSmoothedInput;
+
+    // Smooth the input using the last n samples (which have already been smoothed with the Exponential smoothing)
+    if(m_iAvgSmoothingNumSamples > 1)
+    {
+      m_iBufferIndex = (m_iBufferIndex + 1) % m_iAvgSmoothingNumSamples;
+      m_afAvgBuffer[m_iBufferIndex] = m_fExponentialSmoothedInput;
+      m_fAvgSmoothedInput = 0;
+      for(int i = 0; i < m_iAvgSmoothingNumSamples; ++i)
+      {
+        m_fAvgSmoothedInput += m_afAvgBuffer[i];
+      }
+      m_fAvgSmoothedInput /= m_iAvgSmoothingNumSamples;
+    }
+    else
+    {
+      m_fAvgSmoothedInput = m_fExponentialSmoothedInput;
+    }
+
+    // The output is the result of both types of smoothing scaled by the scaling factor
+    m_fOutput = m_fAvgSmoothedInput * m_fScalingFactor;
+  }
+
+  void Update(int iDeltaMS)
+  {
+    UpdateFromInput(iDeltaMS, m_pNodeInput->GetOutput());
+  }
+
+  float GetOutput()
+  {
+    return m_fOutput;
+  }
+
+/* TEMP_CL - I don't think we need this
+  // 0 = new, 1 = one sample is the past
+  float GetOutputInPast(int iHistoryIndex)
+  {
+    int iPastIndex = m_iPastBufferIndex - iHistoryIndex;
+    while(iPastIndex < 0)
+    {
+      iPastIndex += MAX_NUM_SAMPLES;
+    }
+    return m_afPastBuffer[iPastIndex];
+  }
+  */
+};
+
+
+
+// Tuning vars for singal processing
+static float g_fNode0Exp=0.0;
+static int   g_iNode0Avg=2;
+static float g_fNode1Exp=0.3;
+static int   g_iNode1Avg=10;
+static float g_fDetectThreshold=0.4;
+
+// Signal processing node setup
+ProcessingNode g_oNode0(NULL,     false, g_fNode0Exp, g_iNode0Avg, 1.0); // Input
+ProcessingNode g_oNode1(&g_oNode0, true,  g_fNode1Exp, g_iNode1Avg, 0.6); // 1st serivative
+
+bool g_bDetectOn = false;
+float g_fTriggerVelocity = 0.0;
+
+// Max of the smoothed input so far
+float g_fMaxSmoothedInput = 0.1;
+
+// Clamp function - float
+float ClampF(float fVal, float fMin, float fMax)
+{
+  if(fVal < fMin)
+  {
+    fVal = fMin;
+  }
+  else if(fVal > fMax)
+  {
+    fVal = fMax;
+  }
+
+  return fVal;
+}
+
+void ProcessSignal()
+{
+  float fRawInput = float(g_iSensorValue) / 1024.0; 
+
+  // TEMP_CL - This doesn't match the main loop time right now but I'm just keeping the old value from the PC program for now
+  int iDeltaTimeMS = 10; // TEMP_CL - we might actually get better more consistent results assuming a fixed timestep as opposed to having two loops (the microcontroller and the PC) that sometimes don't sync well.
+  
+  // Update nodes
+  g_oNode0.UpdateFromInput(iDeltaTimeMS, fRawInput);
+  g_oNode1.Update(iDeltaTimeMS);
+  
+  // Simple detection for note on
+  float fSmoothedInput = g_oNode0.GetOutput();
+  float fSmoothedFirstDeriv = g_oNode1.GetOutput();
+  
+  boolean bOldDetectOn = g_bDetectOn;
+  g_bDetectOn = fSmoothedFirstDeriv > g_fDetectThreshold;
+  if(!bOldDetectOn && g_bDetectOn)
+  {
+    // TODO - Find some way to recalibrate this over time as opposed to restarting the app
+    if(fSmoothedInput > g_fMaxSmoothedInput)
+    {
+      g_fMaxSmoothedInput = fSmoothedInput;
+    }
+  
+    g_fTriggerVelocity = ClampF(fSmoothedInput / (g_fMaxSmoothedInput * 0.9), 0.0, 1.0);
+  }
+
+  if(!g_bDetectOn)
+  {
+    g_fTriggerVelocity = 0;
+  }
+}
+
+
+
 void setup()
 {
 	// setup debugging serial port (over USB cable) at 9600 bps
@@ -111,6 +311,8 @@ void setup()
 	// Init timer
 	Timer1.initialize(FREQ_STEP);
 	Timer1.attachInterrupt(DimmerCheck, FREQ_STEP);
+
+
 }
 
 
@@ -124,6 +326,20 @@ void loop()
 		Serial.print("g_iSensorValue=");
 		Serial.println(g_iSensorValue);
 	}
+
+  // Process the raw sensor value
+  ProcessSignal();
+
+  // TEMP_CL - show trigger for now
+  if(g_bDetectOn)
+  {
+    if(bUseSerialForDebugging)
+    {
+      Serial.print("TOUCH!!!!!!!!!!!!!!!!!!!!!!!!!  ");
+      Serial.print("g_fTriggerVelocity=");
+      Serial.println(g_fTriggerVelocity);
+    }
+  }
 
 	// Take raw sensor value and turn it unto a useful input value called fInput
 	float fSensor = 0;
@@ -151,13 +367,19 @@ void loop()
 	if(fInput > 0.05)
 	{
     g_iDimmerBrightness[0] = fInput * 107 + 20; // Incandecent lights don't turn on right away so we need to start above 0
+
+    // TEMP_CL
+    /*
     if(fInput < 0.5) {
       g_iDimmerBrightness[1] = fInput * 50 + 20; // Incandecent lights don't turn on right away so we need to start above 0
     }
     else {
       g_iDimmerBrightness[1] = (1.0-fInput) * 50 + 20; // Incandecent lights don't turn on right away so we need to start above 0
     }
-	}
+    */
+ 	}
+  float g_iDefaultBrightnessLight1 = 50; // TEMP_CL
+  g_iDimmerBrightness[1] = g_iDefaultBrightnessLight1 + g_fTriggerVelocity * (127-g_iDefaultBrightnessLight1);
 
 	if(bUseSerialForDebugging)
 	{
